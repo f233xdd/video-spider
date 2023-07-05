c7bc8ed2c22e4579f816d314cc4e109dbe9589ab
Max <f233xdd@outlook.com>
2023-07-05 12:44:11 +0800

    update

--- spider.py
+++ spider.py
@@ -53,26 +53,34 @@ class Downloader(Spider):
         time_of_404 = 0
         priority = None
         lock = threading.Lock()
+
         while True:
             try:
                 priority, url = next(self._urls)
+
                 with lock:
                     page = requests.get(url=url, timeout=2)
+
                 if page.status_code == 200:
                     self._queue.put((priority, page.content), block=True)  # 在队列中放入列表
                     print(f"Get: {url} | from {self.name}")
+
                 else:
                     raise NetConnectionError(page.status_code)
+
             except NetConnectionError as e:
                 print(f"Fail: {url} | {e} | from {self.name}")
+
                 if e.status_code == 404:
                     time_of_404 += 1
                 if time_of_404 == 2:
                     print(f"Exit[404]! | from {self.name}")
                     break
+
             except requests.exceptions.ReadTimeout:
                 print(f"TimeOut!{url} | from {self.name}")
                 self._queue.put((priority, None), block=True)
+
             except StopIteration:
                 print(f"Exit! | from {self.name}")
                 break
@@ -93,13 +101,7 @@ class Writer(Spider):
             try:
                 priority, data = self._queue.get(block=True, timeout=10)
 
-                if data is None:
-                    print(f"before {priority} {current}")
-
                 if priority == current:
-
-                    if data is None:
-                        print(f"after {priority} {current}")
 
                     with lock:
                         if data is None:
@@ -107,17 +109,21 @@ class Writer(Spider):
                             error_log.write(f"{priority} cannot get.\n")
                             print(f"{priority} cannot get.\n")
                             current += 1
+
                         else:
                             fd.write(data)
                             print(f"Write data[{priority}] | from {self.name}")
                             current += 1
+
                 else:
                     rest.append((priority, data))  # 如果获取的队列元素不合预期，就储存起来
                     rest.sort()
+
             except (TimeoutError, queue.Empty):
                 print(f"TimeOut! | from {self.name}")
                 error_log.close()
                 sys.exit(10)
+
             index = 0
             while index != len(rest):  # 检查有没有符合预期的元素，有就写入数据
                 if rest[index][0] == current:
